03d47ae8dd99dfe7467265200300934a3c1dec79
Zakkery Cales <zakkerycales@gmail.com>
2024-06-19 20:56:21 -0500

    update

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import "expo-dev-client";
 
 // import ScannerScreen from "./app/screens/ScannerScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import QRCodeGenerator from "./screens/QrcodeScreen";
 
 export default function App() {
   return (
-    <Auth0Provider
-      domain={"dev-s0asvbc6jgxomce8.us.auth0.com"}
-      clientId={"rY7s7DW5a4NopwPsTHGQ90H4YTUS1Q73"}
-    >
-      <WelcomeScreen />
-    </Auth0Provider>
+    // <Auth0Provider
+    //   domain={"dev-s0asvbc6jgxomce8.us.auth0.com"}
+    //   clientId={"rY7s7DW5a4NopwPsTHGQ90H4YTUS1Q73"}
+    // >
+    //   <WelcomeScreen />
+    // </Auth0Provider>
+    <QRCodeGenerator />
   );
 }
